4c195124f3430071032598e0c8223e271514b60f
tejb96 <104933640+tejb96@users.noreply.github.com>
2023-06-07 13:12:05 -0600

    feat: analysis button

--- src/TextAnalysisGUI.java
+++ src/TextAnalysisGUI.java
@@ -1,13 +1,73 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 /**
- * @author 
+ * @author Tej
  *
  */
-public class TextAnalysisGUI {
+public class TextAnalysisGUI implements ActionListener {
+	
+	private JTextArea inputField;
+	private JTextArea display;
+	private JButton clear, analysis;
+	private TextAnalyzer textAnalyze;
+	
+	public TextAnalysisGUI(String title) {
+		JFrame jfrm = new JFrame(title);
+		jfrm.setLayout(new FlowLayout());
+		jfrm.setSize(800, 400);  
+		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jfrm.setResizable(false);
+		JLabel prompt = new JLabel("Type or paste your text below.");
+		jfrm.add(prompt);
+		
+		this.inputField = new JTextArea(0, 0);
+        JScrollPane inputScrollPane = new JScrollPane(inputField);
+        inputScrollPane.setPreferredSize(new Dimension(750, 250)); // Set preferred size
+        inputField.setLineWrap(true); // Enable line wrap
+        inputField.setWrapStyleWord(true); // Enable word wrap
+        jfrm.add(inputScrollPane);
+//		this.inputField.addActionListener(this); 
+
+		
+		this.display = new JTextArea (2 ,75);
+		jfrm.add(display);
+		
+		analysis = new JButton("analysis");
+		analysis.addActionListener(this);
+		jfrm.add(analysis);
+
+		clear = new JButton("clear");
+		clear.addActionListener(this);
+		jfrm.add(clear);
+		
+		jfrm.setVisible(true);	
+		
+		
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		textAnalyze = new TextAnalyzer(inputField.getText());
+		if (e.getSource() == analysis) {
+			display.append(textAnalyze.toString());
+		}
+		
+	}
+
 
 	public static void main(String[] args) {
-		
+		//Starting the GUI application
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new TextAnalysisGUI("Text Analyzer");
+			}
+		});		
 
 	}
+	
 
 }
--- src/TextAnalyzer.java
+++ src/TextAnalyzer.java
@@ -4,5 +4,28 @@
  *
  */
 public class TextAnalyzer {
+	private int numOfSentences=0;
+	private int numOfWords=0;
+	private int i = 0;
+	
+	public TextAnalyzer(String text) {
+		while (i<text.length()){
+			if(text.charAt(i)=='.'||text.charAt(i)=='?'||text.charAt(i)=='!') {numOfSentences++;}
+			else if(text.charAt(i)==' '|| text.charAt(i)==',') {numOfWords++;}
+			i++;
+		}
+	}
+	
+	public int getNumOfSentences(){
+		return numOfSentences;
+	}
+	
+	public int numOfWords(){
+		return numOfWords;
+	}
+	
+	public String toString() {
+		return "The number of sentences: "+ numOfSentences + " and the number of words: "+ numOfWords;
+	}
 
 }
